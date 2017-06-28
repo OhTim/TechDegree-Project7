291289def2f106e4a471beede4e6baa0e5161d00
Tim Dalton <timdaltonm@gmail.com>
2017-06-28 16:20:09 -0500

    updated changes

--- js/media.js
+++ js/media.js
@@ -6,4 +6,10 @@ $('video').mediaelementplayer({
   features: ['playpause','current', 'progress', 'volume', 'fullscreen'],
   videoHeight: "100%",
   videoWidth: "100%",
+  startLanguage: 'en',
+  poster: './img/logo3.jpg',
+  showPosterWhenEnded: 'true',
 });
+
+const videoPlayer = document.querySelectorAll('video');
+const span = document.querySelectorAll('.context span');
