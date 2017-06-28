9c213d0688af296574e0e4283be4a51d94567142
Tim Dalton <timdaltonm@gmail.com>
2017-06-28 18:18:40 -0500

    updated changes

--- js/media.js
+++ js/media.js
@@ -11,5 +11,26 @@ $('video').mediaelementplayer({
   showPosterWhenEnded: 'true',
 });
 
-const videoPlayer = document.querySelectorAll('video');
+const videoPlayer = document.getElementsByTagName('video');
 const span = document.querySelectorAll('.context span');
+
+
+function removeClass(array) {
+  for (let i = 0; i < array.length; i += 1)
+    array[i].className = '';
+};
+
+videoPlayer.addEventListener('timeupdate', (e) => {
+  let videoTime = videoPlayer.currentTime;
+
+  removeClass(span);
+
+  for (let i = 0; i < span.length; i += 1) {
+    let caption = span[i];
+    let captionTime = span[i].getAttribute('data-time');
+
+    if (captionTime > videoTime)
+      caption.className = 'highlight';
+      break;
+  }
+});
